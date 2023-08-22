b145d05605ec6c09364bc594a812036f6505a70c
euiyounghwang <marieuig@gmail.com>
2023-08-22 17:03:41 -0500

    Setup poetry env initially

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
+from config.log_config import create_log
 import json
 
 
+logger = create_log()
 app = FastAPI()
 
 class Item(BaseModel):
     process_id: int
+    price: int
+    name: str
     
 @app.get("/")
 # async def root():
@@ -20,6 +24,8 @@ def get(id:str):
 
 
 @app.post("/api")
-async def root(item: Item):
-    print(item, type(item), item.process_id, [{"process_id" : v} for k, v in item])
+async def request_api(item: Item):
+    request_json = [{k : v} for k, v in item]
+    print(item, type(item), item.process_id, request_json)
+    logger.info("request : {}".format(json.dumps(request_json, indent=2)))
     return item
