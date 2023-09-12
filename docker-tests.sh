888ce6c2a78986186b05354cc5247c9cd774f27d
euiyounghwang <marieuig@gmail.com>
2023-09-12 00:08:21 -0500

    Dockerfile touch

--- docker-tests.sh
+++ docker-tests.sh
@@ -6,6 +6,6 @@ SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 #--platform linux/amd64
 # VS Code : Remove -it
-docker run --rm -it --name fn-flask-api-test --publish 1001:8080 --expose 8080 \
+docker run --rm -it --name fn-vector-search-api:test --publish 1001:8080 --expose 8080 \
   -v "$SCRIPTDIR:/app/FN-FTA-Services/" \
   fn-flask-api:test
