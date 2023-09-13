4e87c5f91f5f754d7fbe8dd94f4e2da815d07e45
euiyounghwang <marieuig@gmail.com>
2023-09-13 00:29:10 -0500

    Add Dockerfile & docker-compose.yml with arguments

--- docker-tests.sh
+++ docker-tests.sh
@@ -7,10 +7,10 @@ SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 #--platform linux/amd64
 # VS Code : Remove -it
 # docker run --rm -it
-docker run --rm -it --name fn-vector-search-api-test \
+docker run -i -t --name fn-vector-search-api-test \
   --publish 7002:8080 --expose 8080 \
+  --network bridge \
   -e DATABASE_URL=postgresql://postgres:1234@host.docker.internal:15432/postgres \
   -e ES_HOST=http://host.docker.internal:9209 \
-  --network bridge \
   -v "$SCRIPTDIR:/app/FN-FTA-Services/" \
   fn-vector-search-api:test
