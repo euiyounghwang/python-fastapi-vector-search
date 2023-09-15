4dbfbd44193c8a430567370144dd85d9ba3811b3
euiyounghwang <marieuig@gmail.com>
2023-09-15 16:04:20 -0500

    Update rmq_message shell script

--- rmq_message_send.sh
+++ rmq_message_send.sh
@@ -4,6 +4,11 @@ set -ex
 SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTDIR
 
+# --
+# Conda activate
+# --
+source /Users/euiyoung.hwang/opt/anaconda3/bin/activate fastapi_service
+
 # exec python -m service.Handler.message.test_send_message --es $RABBIT_HOST
 if [ -f "$RABBIT_HOST" ]; then
     python service/Handler/message/test_send_message.py --es $RABBIT_HOST
